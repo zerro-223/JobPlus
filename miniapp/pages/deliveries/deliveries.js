4a578d500fa1f8283c0f4259c47ecad239599733
zerro-223 <zerro-223@users.noreply.github.com>
2026-05-14 19:50:43 +0800

    chore: remove WeChat miniapp (not required)

--- miniapp/pages/deliveries/deliveries.js
+++
@@ -1,7 +0,0 @@
-const app = getApp();
-Page({
-  data: { list: [] },
-  onShow() { this.load(); },
-  load() { app.request('/deliveries').then(list => this.setData({ list: list || [] })); },
-  goDetail(e) { wx.navigateTo({ url: '/pages/detail/detail?id='+e.currentTarget.dataset.id }); }
-});
